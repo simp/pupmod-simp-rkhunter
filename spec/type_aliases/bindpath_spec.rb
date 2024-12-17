063e2e2dbf344ea10135a36f588da9bb9f973b4f
Steven Pritchard <steve@sicura.us>
2024-12-17 09:30:11 -0600

    Clean up for rubocop

--- spec/type_aliases/bindpath_spec.rb
+++ spec/type_aliases/bindpath_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe 'Rkhunter::BindPath' do
   context 'with valid parameters' do
-    it { is_expected.to allow_value( '+/this/is/fine' ) }
-    it { is_expected.to allow_value( '+/this/is/fine /this/too' ) }
-    it { is_expected.to allow_value( '/this/is/fine +/also/fine' ) }
+    it { is_expected.to allow_value('+/this/is/fine') }
+    it { is_expected.to allow_value('+/this/is/fine /this/too') }
+    it { is_expected.to allow_value('/this/is/fine +/also/fine') }
   end
   context 'with invalid parameters' do
-    it { is_expected.not_to allow_value( 'no/bueno' ) }
-    it { is_expected.not_to allow_value( '../no/bueno/still' ) }
-    it { is_expected.not_to allow_value( '*/still/no/bueno/' ) }
+    it { is_expected.not_to allow_value('no/bueno') }
+    it { is_expected.not_to allow_value('../no/bueno/still') }
+    it { is_expected.not_to allow_value('*/still/no/bueno/') }
   end
   context 'with silly things' do
-    it { is_expected.not_to allow_value( [] ) }
-    it { is_expected.not_to allow_value( '.' ) }
-    it { is_expected.not_to allow_value( '' ) }
-    it { is_expected.not_to allow_value( :undef ) }
+    it { is_expected.not_to allow_value([]) }
+    it { is_expected.not_to allow_value('.') }
+    it { is_expected.not_to allow_value('') }
+    it { is_expected.not_to allow_value(:undef) }
   end
 end
